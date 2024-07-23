889598fb40ce4423f5db880195c599fb9646a754
Mihai <mihairuse123@yahoo.com>
2024-07-23 12:41:23 +0300

    first commit

--- test.js
+++ test.js
@@ -79,20 +79,6 @@ overlay4.addEventListener('click', () => {
 });
 
 
-let slideIndex = 0;
-
-
-function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("slide");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    slides[slideIndex-1].style.display = "flex";  
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-  }
 
 
 
